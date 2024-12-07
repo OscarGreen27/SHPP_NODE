5b365dca467b52d19f77f676ca301aedd3409dec
OscarGreen27 <andrii.voitenko97@gmail.com>
2024-12-07 19:50:46 +0200

    Update Product.js

--- 1/1.1/Product.js
+++ 1/1.1/Product.js
@@ -71,105 +71,106 @@ function Product(id, name, description, price, brand, activeSize, quantity, date
             default: console.log("Name has not bee change");
         }
     }
-}
 
-this.getDescriptoion = function () {
-    return this.description;
-}
 
-this.setDescription = function (newDescripton) {
-    switch (typeof newDescripton) {
-        case "string": this.description = newDescripton;
-        default: console.log("Description has not bee change");
+    this.getDescriptoion = function () {
+        return this.description;
     }
-}
 
-this.getPrice = function () {
-    return this.price;
-}
-
-this.setPrice = function (newPrice) {
-    switch (typeof newPrice) {
-        case "number": this.price = newPrice;
-        default: console.log("Price has not bee change");
+    this.setDescription = function (newDescripton) {
+        switch (typeof newDescripton) {
+            case "string": this.description = newDescripton;
+            default: console.log("Description has not bee change");
+        }
     }
-}
 
-this.getBrand = function () {
-    return this.brand;
-}
-
-this.setBrand = function (newBrand) {
-    switch (typeof newBrand) {
-        case "string": this.brand = newBrand;
-        default: console.log("Brand has not bee change");
+    this.getPrice = function () {
+        return this.price;
     }
-}
 
-this.getSizes = function () {
-    return this.sizes;
-}
-
-this.setSize = function (newSize) {
-    switch (typeof newSize) {
-        case "string": this.sizes.push(newSize);
-        default: console.log("New size not added");
+    this.setPrice = function (newPrice) {
+        switch (typeof newPrice) {
+            case "number": this.price = newPrice;
+            default: console.log("Price has not bee change");
+        }
     }
-}
 
-this.getActiveSize = function () {
-    return this.activeSize;
-}
-
-this.setActiveSize = function (newActiveSize) {
-    switch (typeof newActiveSize) {
-        case "string": this.activeSize = newActiveSize;
+    this.getBrand = function () {
+        return this.brand;
     }
-}
 
-this.getQuantity = function () {
-    return this.quantity;
-}
-
-this.setQuantity = function (newQuantity) {
-    switch (typeof newQuantity) {
-        case "string": this.quantity = newQuantity;
-        default: console.log("Quantity has not bee change");
+    this.setBrand = function (newBrand) {
+        switch (typeof newBrand) {
+            case "string": this.brand = newBrand;
+            default: console.log("Brand has not bee change");
+        }
     }
-}
 
-this.getDate = function () {
-    return this.date;
-}
-
-this.setDate = function (newDate) {
-    if (newDate instanceof Date) {
-        this.date = newDate;
-    } else {
-        console.log("Date has not bee change");
+    this.getSizes = function () {
+        return this.sizes;
     }
-}
 
-this.getReviews = function () {
-    return this.reviews;
-}
-
-this.setReview = function (Review) {
-    if (review instanceof Review) {
-        this.reviews.push(Review);
-    } else {
-        console.log("No review added.");
+    this.setSize = function (newSize) {
+        switch (typeof newSize) {
+            case "string": this.sizes.push(newSize);
+            default: console.log("New size not added");
+        }
     }
-}
 
-this.getImages = function () {
-    return this.images;
-}
+    this.getActiveSize = function () {
+        return this.activeSize;
+    }
 
-this.setImage = function (image) {
-    switch (typeof image) {
-        case "string": this.images.push(image);
-        default: console.log("No image added.")
+    this.setActiveSize = function (newActiveSize) {
+        switch (typeof newActiveSize) {
+            case "string": this.activeSize = newActiveSize;
+        }
+    }
+
+    this.getQuantity = function () {
+        return this.quantity;
+    }
+
+    this.setQuantity = function (newQuantity) {
+        switch (typeof newQuantity) {
+            case "string": this.quantity = newQuantity;
+            default: console.log("Quantity has not bee change");
+        }
+    }
+
+    this.getDate = function () {
+        return this.date;
+    }
+
+    this.setDate = function (newDate) {
+        if (newDate instanceof Date) {
+            this.date = newDate;
+        } else {
+            console.log("Date has not bee change");
+        }
+    }
+
+    this.getReviews = function () {
+        return this.reviews;
+    }
+
+    this.setReview = function (Review) {
+        if (review instanceof Review) {
+            this.reviews.push(Review);
+        } else {
+            console.log("No review added.");
+        }
+    }
+
+    this.getImages = function () {
+        return this.images;
+    }
+
+    this.setImage = function (image) {
+        switch (typeof image) {
+            case "string": this.images.push(image);
+            default: console.log("No image added.")
+        }
     }
 }
 
