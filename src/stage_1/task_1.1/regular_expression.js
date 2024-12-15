3dc375f26b551ac28dcc5793ea0754f5bc3ee37d
OscarGreen27 <andrii.voitenko97@gmail.com>
2024-12-15 17:00:15 +0200

    add coments

--- src/stage_1/task_1.1/regular_expression.js
+++ src/stage_1/task_1.1/regular_expression.js
@@ -1,8 +1,14 @@
+/**
+ * The object contains fields that store regular expressions.
+ */
 let reguralExpression = {
-    
-    emailValidator: new RegExp("^[a-zA-Z0-9]{1, 20}@[a-zA-Z0-9]{1, 15}\\.(a-z){1-5}$"),
 
-    phoneValidator: new RedExp("^(\+38\s?)?(?\d{3}?[\s-]?)?\d{3}[\s-]?\d{2}[\s-]?\d{2}$"),
+    //Regular expression for email validation
+    emailValidator: new RegExp("^[a-zA-Z0-9][a-zA-Z0-9.+-]{1,19}@[a-zA-Z0-9.!$%&'*+/=?^_-]{1,15}\\.[a-z]{1,5}$"),
 
-    passwordValidator: new RedExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d_]{8,}$"),
+    //Regular expression for phone number validation
+    phoneValidator: new RegExp("^(\\+38\\s?)?(\\(\\d{3}\\)|\\d{3})[\\s-]?(\\d{3})[\\s-]?(\\d{2})[\\s-]?(\\d{2})$"),
+
+    //Regular expression for passvord validation
+    passwordValidator: new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d_]{8,}$"),
 }
